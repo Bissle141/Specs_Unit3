b9ebf69e0120b4a9a5309574b9163c3213fe8684
Alyssa <alyssa.black141@gmail.com>
2023-01-04 18:18:17 -0700

    part 2

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, flash, request
 import jinja2
+import melons
 
 app = Flask(__name__)
 app.jinja_env.undefined = jinja2.StrictUndefined  # for debugging purposes
@@ -12,13 +13,16 @@ def homepage():
 def all_melons():
    """Return a page listing all the melons available for purchase."""
 
-   return render_template("all_melons.html")
+   melon_list = melons.get_all()
+   return render_template("all_melons.html", melon_list=melon_list)
 
 @app.route("/melon/<melon_id>")
 def melon_details(melon_id):
    """Return a page showing all info about a melon. Also, provide a button to buy that melon."""
 
-   return render_template("melon_details.html")
+   detailed_melon = melons.melon_look_up(melon_id)
+   
+   return render_template("melon_details.html", melon=detailed_melon)
 
 @app.route("/add_to_cart/<melon_id>")
 def add_to_cart(melon_id):
